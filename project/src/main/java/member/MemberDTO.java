e013852e404d7448676713ba2bbe81f0fafae972
ksh <aiostpch@naver.com>
2024-09-19 16:47:21 +0900

    dto

---
+++ project/src/main/java/member/MemberDTO.java
@@ -0,0 +1,106 @@
+package member;
+
+public class MemberDTO {
+	
+	private String role;
+	private String id;
+	private String memberId;
+	private String memberPw;
+	private String name;
+	private String birth;
+	private String phoneNumber;
+	private String email;
+	private String address;
+	private String nickName;
+	private String memberOut;
+	private String createDate;
+	private String reportYn;
+	private String blockYn;
+	
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getMemberPw() {
+		return memberPw;
+	}
+	public void setMemberPw(String memberPw) {
+		this.memberPw = memberPw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getMemberOut() {
+		return memberOut;
+	}
+	public void setMemberOut(String memberOut) {
+		this.memberOut = memberOut;
+	}
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getReportYn() {
+		return reportYn;
+	}
+	public void setReportYn(String reportYn) {
+		this.reportYn = reportYn;
+	}
+	public String getBlockYn() {
+		return blockYn;
+	}
+	public void setBlockYn(String blockYn) {
+		this.blockYn = blockYn;
+	}
+	
+	
+}
